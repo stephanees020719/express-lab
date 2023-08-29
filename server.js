f158df063abbb88df3d38473294a7033cb7f5371
stephanees020719 <stephanees020719@gmail.com>
2023-08-29 16:07:59 -0400

    magic

--- server.js
+++ server.js
@@ -33,12 +33,16 @@ app.get('/tip/:total/:tipPercentage', function(req, res) {
 
 
 //magic
-app.get('/magic',function(req, res){
-    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+// EightBall Array: Random Response
+app.get('/magic', function(req, res) {
+    // Create responses array
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
+    // BIG UPS STEFON! (literally the last minute of our breakout rooms!)
     let randomResponse = responses[Math.floor(Math.random()*responses.length)]
+
+    res.send(`<h1>Will I be a Millionaire? <br> Magic Eight Ball Says: "${randomResponse}"</h1>`);
 });
-res.send(`<h1>Will I be a Millionaire? ${randomResponse}</h1>`)
 
 
 
