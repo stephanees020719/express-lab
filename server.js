410ac6120623432cce82a934cdc7ed3db4708d54
stephanees020719 <stephanees020719@gmail.com>
2023-08-29 16:04:34 -0400

    magic

--- server.js
+++ server.js
@@ -29,6 +29,19 @@ app.get('/tip/:total/:tipPercentage', function(req, res) {
     res.send(`Your tip amount is: ${tipAmount}`);
 });
 
+
+
+
+//magic
+app.get('/magic',function(req, res){
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    let randomResponse = responses[Math.floor(Math.random()*responses.length)]
+});
+res.send(`<h1>Will I be a Millionaire? ${randomResponse}</h1>`)
+
+
+
 // Tell the app to listen on port 3000
 
 app.listen(3000, function () {
